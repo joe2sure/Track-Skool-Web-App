14d589f8ddddfe6befc7fcf9f3c0c6f8b6ab5078
joe2sure <joe2sure1@gmail.com>
2025-08-18 13:37:14 +0100

    fix: resolve Button2 and Card2 import issues

--- app/assignments/page.tsx
+++ app/assignments/page.tsx
@@ -2,8 +2,6 @@
 
 import { useState } from 'react';
 import Header from '../../components/Header';
-// import Card from '../../components/ui/card';
-// import Button from '../../components/ui/button';
 import Input from '../../components/ui/Input';
 import { ThemeProvider } from '../../components/ThemeProvider';
 import Modal from '../../components/ui/Modal';
